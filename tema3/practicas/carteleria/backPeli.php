1761f75802f221eed4f7de927ecfb5132e45c59f
MªCarmenAlfaro <maricarmenalfaro1616@gmail.com>
2023-01-10 20:53:55 +0100

    comienzo

--- tema3/practicas/carteleria/backPeli.php
+++ tema3/practicas/carteleria/backPeli.php
@@ -1,8 +1,7 @@
 
 
 <?php
-     ini_set('display_errors', 'On');
-     ini_set('html_errors', 0);
+
 pintarPeli(contenidoPeli());
 
 function contenidoPeli()
---
+++ tema4/torneo/pantalla.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Torneo</title>
+    <link rel="stylesheet" href="css/estiloFinal.css">
+</head>
+
+<body>
+    <h1>Torneo tenis mesa</h1>
+    <h2>Temporada 2023</h2>
+    <div class="tabla">
+
+
+        <div class="columnas">
+            Cuartos
+            <div class="cuartos">
+                <div class="jugadorCuarto">Prueba</div>
+                <div class="jugadorCuarto">Prueba</div>
+                <div class="jugadorCuarto">Prueba</div>
+                <div class="jugadorCuarto">Prueba</div>
+                <div class="jugadorCuarto">Prueba</div>
+                <div class="jugadorCuarto">Prueba</div>
+                <div class="jugadorCuarto">Prueba</div>
+                <div class="jugadorCuarto">Prueba</div>
+            </div>
+        </div>
+
+
+
+
+
+        <div class="columnas">
+            Semifinal
+            <div class="semifinal">
+                <div class="jugadorSemi">Prueba</div>
+                <div class="jugadorSemi">Prueba</div>
+                <div class="jugadorSemi">Prueba</div>
+                <div class="jugadorSemi">Prueba</div>
+            </div>
+        </div>
+
+
+        <div class="columnas">
+            Final
+        <div class="final">
+                <div class="jugadorFinal">Prueba</div>
+                <div class="jugadorFinal">Prueba</div>
+                <div class="jugadorFinal">Prueba</div>
+                <div class="jugadorFinal">Prueba</div>
+            </div>
+        </div>
+
+
+        <div class="columnas">
+            Campeón
+            <div class="campeon">Prueba</div>
+        </div>
+
+    </div>
+    </div>
+</body>
+
+</html>
